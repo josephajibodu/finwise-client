7109544e5df1186f1e8112df2e7d68b2a8d03486
Joseph Ajibodu <josephajibodu@gmail.com>
2026-01-16 19:54:56 +0100

    feat: tawkto added

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,24 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </Context>
           <GlobalEffectsProvider />
         </div>
+        {/*Start of Tawk.to Script*/}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/696a8848eded4f198040629e/1jf428mv3';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+            `,
+          }}
+        />
+        {/*End of Tawk.to Script*/}
       </body>
     </html>
   );
